015d28bcee6e8c8e0fb9270e9ba24c93ef6e80e6
Ubuntu <ubuntu@ip-172-31-24-149.eu-central-1.compute.internal>
2017-04-19 16:59:14 +0000

    Fixed external link and build errors

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ cp -r keys/certs staging/ethereum-authentication-server/config
 cp -r keys/rsa_keys staging/ethereum-authentication-server/config
 cp -r keys/certs staging/ethereum-sample-web/node-server/config
 cp -r keys/rsa_keys staging/ethereum-sample-web/node-server/config
-
+mkdir staging/ethereum-sample-web/node-server/config/certs
+mv staging/ethereum-sample-web/node-server/config/server* staging/ethereum-sample-web/node-server/config/certs
 docker build -t npm-dependencies ./staging/npm-dependencies
 docker build -t go-ethereum ./staging/go-ethereum
 docker-compose build
