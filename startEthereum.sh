dc2a600bf7fcb4a53d570d27226c5b4e0d056fb2
Ubuntu <ubuntu@ip-172-31-24-149.eu-central-1.compute.internal>
2017-05-12 12:25:30 +0000

    Migrated to rinkeby testnet

--- startEthereum.sh
+++ startEthereum.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 docker run -p 8545:8545 --name go-ethereum go-ethereum /bin/bash /start.sh &
-
-echo "Waiting for the node to go up, please wait..."
-sleep 120
-
-curl -X POST -d @node1 localhost:8545
-curl -X POST -d @node2 localhost:8545
